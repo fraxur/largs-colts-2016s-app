45150559c2d4ced028cc5b7c1fa0e0c8f91df40b
Martin Fraser <fraxur@outlook.com>
2026-05-19 07:46:11 +0100

    Add files via upload

---
+++ www/firebase-sw-config.js
@@ -0,0 +1,11 @@
+self.largsFirebaseMessagingConfig = {
+    enabled: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyDpavXw2gRiAAWLTGRLyNgoqMgOUbQ9hwM",
+        authDomain: "largs-colts-2016s-app-c8909.firebaseapp.com",
+        projectId: "largs-colts-2016s-app-c8909",
+        storageBucket: "largs-colts-2016s-app-c8909.firebasestorage.app",
+        messagingSenderId: "517470226181",
+        appId: "1:517470226181:web:d5d6804849f5552e7f596c"
+    },
+};
