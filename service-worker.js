2684580ced98d971d0c3dcc8d6d5c965907f3290
Martin Fraser <fraxur@outlook.com>
2026-08-20 13:57:25 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("firebase-sw-config.js?v=live-29");
+importScripts("firebase-sw-config.js?v=live-30");
 
 if (self.largsFirebaseMessagingConfig?.enabled) {
   importScripts("https://www.gstatic.com/firebasejs/10.12.5/firebase-app-compat.js");
@@ -16,7 +16,7 @@ if (self.largsFirebaseMessagingConfig?.enabled) {
   });
 }
 
-const cacheName = "largs-colts-live-29";
+const cacheName = "largs-colts-live-30";
 const appShell = [
   "./",
   "index.html",
